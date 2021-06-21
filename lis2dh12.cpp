80ea7374c19044c87909d21614122ccd0d8c9d18
Gean Santos <geans.santos@gmail.com>
2021-06-21 17:24:18 -0300

    Update lis2dh12 sources. - Add get_error function - Change return error to return values or not return.

--- lis2dh12.cpp
+++ lis2dh12.cpp
@@ -5,99 +5,54 @@
 #include <fcntl.h>      // open
 #include <unistd.h>     // usleep, write, read, close
 
-/* Public methods */
 
-LIS2DH12::LIS2DH12(const char *pathDevice, PowerMode powerMode, ConnectionMode conn){
-    int filedescriptor;
-    stmdev_ctx_t ctx;
-
-    switch(conn) {
-        case ConnectionMode.I2C:
-            ctx.write_reg = i2c_write;
-            ctx.read_reg = i2c_read;
-            filedescriptor = i2cOpen(pathDevice);
-            break;
-        case ConnectionMode.SPI:
-            ctx.write_reg = spi_write;
-            ctx.read_reg = spi_read;
-            filedescriptor = spiOpen(pathDevice);
-            spiMaxSpeed = 100000; // study other best number
-            spiMode = 0; // study other best number
-            break;
-    }
-
-    if (filedescriptor > 0) {
-        ctx.handle = (void*)&filedescriptor;
-        ctxDevice = &ctx;
-        configPowerMode(powerMode);
-    } else {
-        // TODO: check and alert error
-    }
+void LIS2DH12::enable(lis2dh12_odr_t odr){
+    error_status = lis2dh12_data_rate_set(ctx, odr);
 }
 
+void LIS2DH12::disable(){
+    error_status = lis2dh12_data_rate_set(ctx, LIS2DH12_POWER_DOWN);
+}
 
-int LIS2DH12::getAccelerationRaw(int16_t *x, int16_t *y, int16_t *z){
+int16_t* LIS2DH12::acceleration_raw(){
     int16_t acc[3];
-    int ret = lis2dh12_acceleration_raw_get(ctxDevice, acc);
-    x = acc;
-    y = acc + 1;
-    z = acc + 2;
-    return ret;
+    error_status = lis2dh12_acceleration_raw_get(ctx, acc);
+    return acc;
 }
 
-int LIS2DH12::getTemperatureRaw(int16_t *temp){
+int16_t LIS2DH12::x_axis(){
+    int16_t *acc = acceleration_raw();
+    return acc[0];
+}
+
+int16_t LIS2DH12::y_axis(){
+    int16_t *acc = acceleration_raw();
+    return acc[1];
+}
+
+int16_t LIS2DH12::z_axis(){
+    int16_t *acc = acceleration_raw();
+    return acc[2];
+}
+
+int16_t LIS2DH12::temperature_raw(){
     int16_t tmp;
-    int ret = lis2dh12_temperature_raw_get(ctxDevice, tmp);
-    temp = &tmp;
-    return ret;
+    error_status = lis2dh12_temperature_raw_get(ctx, &tmp);
+    return tmp;
 }
 
-char LIS2DH12::getInterrupt_1(){return -1;} // TODO
-
-char LIS2DH12::getInterrupt_2(){return -1;} // TODO
-
-int LIS2DH12::configPowerMode(PowerMode powerMode){return -1;} // TODO
-
-int LIS2DH12::configInterruption(InterruptMode interruptMode){return -1;} // TODO
-
-/* Private methods */
-
-int LIS2DH12::initDevice(const char *pathDevice){
-    return open(pathDevice, O_RDWR);
+void LIS2DH12::config_interruption(lis2dh12_ctrl_reg3_t val){
+    error_status = lis2dh12_pin_int1_config_set(ctx, val);
 }
 
-int32_t LIS2DH12::i2c_write(void *handle, uint8_t reg, const uint8_t *data, uint16_t len){
-    int ret = i2cWrite((int)*handle, LIS2DH12_ID, reg, data, len);
-    return (ret > 0) 0 : ret;
+lis2dh12_int1_src_t LIS2DH12::interrupt_1(){
+    lis2dh12_int1_src_t val;
+    error_status = lis2dh12_int1_gen_source_get(ctx, &val);
+    return val;
 }
 
-int32_t LIS2DH12::i2c_read(void *handle, uint8_t reg, uint8_t *data, uint16_t len){
-    int ret = i2cRead((int)*handle, LIS2DH12_ID, reg, data, len);
-    return (ret > 0) 0 : ret;
-}
-
-int32_t LIS2DH12::spi_write(void *handle, uint8_t reg, const uint8_t *data, uint16_t len){
-    uint8_t config = 0x02; // write mode (bit 0 = 0); auto incremented address (bit 1 = 1)
-    config |= (reg << 2);
-    len += 1;
-    uint8_t rx[len];
-    uint8_t tx[len];
-    tx[0] = config;
-    tx+1 = data;
-
-    return spiTransfer((int)*handle, tx, rx, len, spiMaxSpeed, spiMode);
-}
-
-int32_t LIS2DH12::spi_read(void *handle, uint8_t reg, uint8_t *data, uint16_t len){
-    uint8_t config = 0x03; // read mode (bit 0 = 1); auto incremented address (bit 1 = 1)
-    config |= (reg << 2);
-    len += 1;
-    uint8_t rx[len];
-    uint8_t tx[len];
-    tx[0] = config;
-
-    // TODO: test if len should be incremented or not, and if the received data starts at rx[0] or rx[1]
-    int ret spiTransfer((int)*handle, tx, rx, len, spiMaxSpeed, spiMode);
-    data = rx
-    return ret;
+lis2dh12_int2_src_t LIS2DH12::interrupt_2(){
+    lis2dh12_int2_src_t val;
+    error_status = lis2dh12_int2_gen_source_get(ctx, &val);
+    return val;
 }
--- lis2dh12.hpp
+++ lis2dh12.hpp
@@ -3,56 +3,41 @@
 
 #include "lis2dh12_reg.h"
 
-enum PowerMode {
-    down, low, normal, high
-};
-
-enum InterruptMode {
-    OR_COMBINATION        = 0X00,
-    MOVEMENT_RECOGNITION  = 0X40,
-    AND_COMBINATION       = 0x80,
-    POSTITION_RECOGNITION = 0xC0
-};
-
-enum ConnectionMode {
-    I2C, SPI
-};
 
 class LIS2DH12 {
 public:
-    LIS2DH12(const char*, PowerMode, ConnectionMode);
-    ~LIS2DH12() {
-        close(getfd());
+    LIS2DH12(lis2dh12_op_md_t op, stmdev_write_ptr wr, stmdev_read_ptr rd) {
+        operation_mode = op;
+
+        stmdev_ctx_t ctx_tmp;
+        ctx_tmp.write_reg = wr;
+        ctx_tmp.read_reg = rd;
+        ctx = &ctx_tmp;
+
+        error_status = 0;
+    }
+
+    ~LIS2DH12(){}
+
+    int get_error() {
+        return error_status;
     }
     
-    bool isOpen(){
-        return getfd() > 0;
-    }
-    int getAccelerationRaw(int16_t *x, int16_t *y, int16_t *z);
-    int getTemperatureRaw(int16_t *temp);
-    char getInterrupt_1();
-    char getInterrupt_2();
-    int configPowerMode(PowerMode);
-    int configInterruption(InterruptMode);
+    void enable(lis2dh12_odr_t odr);
+    void disable();
+    int16_t* acceleration_raw();
+    int16_t x_axis();
+    int16_t y_axis();
+    int16_t z_axis();
+    int16_t temperature_raw();
+    void config_interruption(lis2dh12_ctrl_reg3_t);
+    lis2dh12_int1_src_t interrupt_1();
+    lis2dh12_int2_src_t interrupt_2();
 
 private:
-    stmdev_ctx_t *ctxDevice;
-    
-    int getfd(){
-        if (ctxDevice != NULL && ctxDevice->handle != NULL) {
-            return (int)*(ctxDevice->handle);
-        }
-        return -1;
-    }
-    int initDevice(const char*);
-
-    int32_t i2c_write(void *handle, uint8_t reg, const uint8_t *data, uint16_t len);
-    int32_t i2c_read (void *handle, uint8_t reg, uint8_t *data, uint16_t len);
-    int32_t spi_write(void *handle, uint8_t reg, const uint8_t *data, uint16_t len);
-    int32_t spi_read (void *handle, uint8_t reg, uint8_t *data, uint16_t len);
-
-    unsigned int spiMaxSpeed = 0;
-    int spiMode = 0;
+    stmdev_ctx_t *ctx;
+    lis2dh12_op_md_t operation_mode;
+    int error_status;
 };
 
 #endif // LIS2DH12_DRIVER
